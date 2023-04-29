fe7b70ab34aa6ef0ff4fea52a7a14927a6429dc7
mellonaut <rleeazw@outlook.com>
2023-04-29 14:33:43 -0400

    Choco Analyst

--- choco-analyst.ps1
+++ choco-analyst.ps1
@@ -1,3 +1,5 @@
+# Builds on Top of Choco-Proxy
+
 # Install boxstarter with chocolatey and basic configuration
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -24,26 +26,27 @@ choco install burp-suite-community
 choco install firefox
 
 # Proxy Tools
-
 $tools = "C:\Tools"
 mkdir $tools
 cd $tools
 
 # Fireprox
-git clone https://github.com/ustayready/fireprox
+git clone https://github.com/mellonaut/fireprox
 cd fireprox
 virtualenv -p python3 .
 # source bin/activate
 pip install -r requirements.txt
 python fire.py
 
-# Analyst Tools
 
+# Analyst Tools
 # Packages
+choco install nmap
 choco install feroxbuster
 choco install sysinternals --params "/InstallDir:C:\tools\sysinternals"
 choco install die
 choco install pebear
+choco install hollowshunter
 choco install pestudio
 
 # IPSee
@@ -54,3 +57,16 @@ git clone https://github.com/mellosec/IPsee
 cd $tools
 git clone https://github.com/sans-blue-team/DeepBlueCLI.git
 
+# Shodan
+cd $tools
+git clone https://github.com/simeononsecurity/Shodan_PS
+
+# VT
+cd $tools
+git clone https://github.com/simeononsecurity/VirusTotal-PS
+
+# RSAT Offline
+cd $tools
+git clone https://github.com/simeononsecurity/Offine-PS-ActiveDirectory-Install
+
+# Choco-Hunter or Responder will add on from here 
